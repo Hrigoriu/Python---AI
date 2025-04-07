663f6dafe0e9b84ce76e5eb3b36a7e00e309afa6
Hrigoriu Sharov <Hrigoriu@gmail.com>
2025-04-07 19:36:07 +0300

    Another main commit

--- test.py
+++ test.py
@@ -1,5 +1,7 @@
 def hello(name):  # визначення функції
     print("Привіт,", name)  # тіло функції
+    print("Привіт,", name)
+    print("Привіт,", name)
 name = input("Введіть своє ім'я: ")
 hello(name)  # виклик функції
 
