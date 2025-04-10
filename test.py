4e0d7d6e5a0ff846d319b0016c9f6b0c7384b6f9
Hrigoriu Sharov <Hrigoriu@gmail.com>
2025-04-10 23:41:32 +0300

    Алгоритм пошуку та сортування

--- test.py
+++
@@ -1,7 +0,0 @@
-def hello(name):  # визначення функції
-    print("Привіт,", name)  # тіло функції
-    print("Як настрій")
-    print("Привіт,", name)
-    print("Привіт,", name)
-name = input("Введіть своє ім'я: ")
-hello(name)  # виклик функції
